26bb306d16ba60ab4ce3529ad177f707f90c7260
jchaselubitz <jchaselubitz@gmail.com>
2019-01-18 14:39:13 +0000

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,13 @@
-# Create your Costume class here
+class Costume < ActiveRecord::Base
+
+    # def change
+    #     create_class :costumes do |t|
+    #         t.string :name
+    #         t.float :price
+    #         t.string :size
+    #         t.string :image_url
+    #         t.timestamp
+    #     end
+    # end
+end# Create your Costume class here
 # It should inherit from ActiveRecord::Base
